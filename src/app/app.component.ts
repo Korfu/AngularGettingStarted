854b750983b7965dbd58e59373fbf874b68f95aa
Konrad Korf <konrad.korf@infotjenester.no>
2018-11-07 09:51:06 +0100

    basic view template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({ 
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `<div><h1>{{pageTitle}}</h1>
+              <pm-products></pm-products>
+             </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Angular: Getting Started';
+  pageTitle: string = 'Angular: Getting Started';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './prodcuts/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent,
+                  ProductListComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
