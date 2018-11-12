631738253c8cb78e340d50c8a4b75facbe0fee4b
korfu92 <konrad.korf@gmail.com>
2018-11-12 20:32:34 +0100

    pipes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './prodcuts/product-list.component';
+;
 
 @NgModule({
   declarations: [AppComponent,
-                  ProductListComponent],
-  imports: [BrowserModule],
+                 ProductListComponent],
+  imports: [BrowserModule,
+            FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
